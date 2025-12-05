dd9293acec9d2d2e4a85a7e7bd490503f34cca85
jdavis-132 <jdavis132@huskers.unl.edu>
2025-12-05 09:51:56 -0600

    Begin RF work

--- src/R/analyzeLVs.R
+++ src/R/analyzeLVs.R
@@ -1,5 +1,10 @@
 library(tidyverse)
+library(readxl)
 source('src/R/Functions.R')
+
+manual_scores <- read_excel('data/manual/DataRecord_2025_combined (1).xlsx', sheet = '828', skip = 1, 
+                            col_names = c('plotNumber', 'rep', 'date', 'genotype', 
+                                          'score_A', 'score_B', 'score_average', 'notes'))
 
 embeddings_sam <- read_csv('src/sam2/sam2_image_embeddings.csv')
 lv_cols <- colnames(embeddings_sam[2:ncol(embeddings_sam)])
@@ -29,7 +34,15 @@ for(lv in pcs)
   printHistogram(sam_pcs_winsor, lv, title = lv)
 }
 
-embeddings_dino <- read_csv('dinov2_features.csv')
+embeddings_dino <- read_csv('output/dinov2_features.csv')
+embeddings_dino_metadata <- embeddings_dino %>% 
+  mutate(plotNumber = str_split_i(image_path, fixed('/'), 6) %>% 
+           str_split_i('_', 1) %>% 
+           as.numeric()) %>% 
+  left_join(manual_scores, join_by(plotNumber)) %>% 
+  relocate(image_path, plotNumber, rep, date, genotype, score_A, score_B, score_average)
+write_csv(embeddings_dino_metadata, 'output/dinov2_features_metadata.csv')
+
 lv_cols <- colnames(embeddings_dino)[2:ncol(embeddings_dino)]
 for(lv in lv_cols)
 {
