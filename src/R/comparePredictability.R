455850fdedf0215673fad6762d70aa7a1997779f
jdavis-132 <jdavis132@huskers.unl.edu>
2026-02-01 16:36:28 -0600

    compare predictability after final RF run

--- src/R/comparePredictability.R
+++ src/R/comparePredictability.R
@@ -2,18 +2,24 @@ library(tidyverse)
 library(paletteer)
 source('src/R/Functions.R')
 
-model_list <- c('dinov2', 'dinov2', 'sam3', 'sam3', 'ae1', 'ae1', 'ae5', 'ae5', 'ae7', 'ae7', 'ae8', 'ae8', 'ae3', 'ae3', 'ae4', 'ae4', 
-                'ae6', 'ae6')
-predictor_list <- c('feature', 'PC', 'embedding', 'PC', 'latent_dim', 'PC', 'latent_dim', 'PC', 'latent_dim', 'PC', 'latent_dim', 'PC',
-                    'latent_dim', 'PC', 'latent_dim', 'PC', 'latent_dim', 'PC')
-model_descriptors <- c('DINOv2\nFeatures', 'DINOv2\nFeature PCs', 'SAM3\nEmbeddings', 'SAM3\nEmbedding PCs', 'AE1\nLVs', 
-                       'AE1\n LV PCs','AE5\nLVs', 'AE5\nLV PCs', 'AE7\nLVs','AE7\nLV PCs', 'AE8\nLVs', 'AE8\nLV PCs', 'AE3\nLVs', 'AE3\nLV PCs', 
-                       'AE4\nLVs', 'AE4\nLV PCs', 'AE6\nLVs', 'AE6\nLV PCs')
-
-for(i in c(1:18))
+model_list <- c('sam3_mean', 'sam3_std', 'sam3', 'dinov2', 'ae1', 'ae3', 'ae4', 'ae5', 'ae6', 'ae7', 'ae8', 'sam3_mean',
+                'sam3_std', 'sam3', 'dinov2', 'ae1', 'ae3', 'ae4', 'ae5', 'ae6', 'ae7', 'ae8', 'sam3_mean_rs',
+                'sam3_std_rs', 'sam3_rs', 'dinov2_rs', 'ae1_rs', 'ae3_rs', 'ae4_rs', 'ae5_rs', 'ae6_rs', 'ae7_rs',
+                'ae8_rs', 'sam3_mean_rs', 'sam3_std_rs', 'sam3_rs', 'dinov2_rs', 'ae1_rs', 'ae3_rs', 'ae4_rs', 
+                'ae5_rs', 'ae6_rs', 'ae7_rs', 'ae8_rs')
+predictor_list <- c('embedding_mean', 'embedding_std', 'embedding', 'feature', rep('latent_dim', 7), rep('PC', 11), 'embedding_mean', 
+                    'embedding_std', 'embedding', 'feature', rep('latent_dim', 7), rep('PC', 11))
+label_list <- rep(c('all', 'senesced_removed'), each = 22)
+  
+  
+  
+  
+for(i in c(1:44))
 {
-  df <- read_csv(str_c('output/', model_list[i], '_', predictor_list[i], '_pctd_predictions_rf.csv'))
-  getRFPredictability(df, model_descriptor = model_descriptors[i])
+  df <- read_csv(str_c('output/rf/predictions/', model_list[i], '_', predictor_list[i], '_pctd_', label_list[i], '_predictions_rf.csv'))
+  getRFPredictability(df, model_descriptor = str_c(model_list[i], predictor_list[i], sep = ':'))
 }
+
+
 
 
