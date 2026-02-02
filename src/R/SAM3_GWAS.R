5c8b76ca8b56fbb7c8807054c9251be29d25e673
jdavis-132 <jdavis132@huskers.unl.edu>
2026-02-02 09:04:53 -0600

    farmcpu analysis

--- src/R/SAM3_GWAS.R
+++ src/R/SAM3_GWAS.R
@@ -3,18 +3,31 @@ source('src/R/Functions.R')
 library(paletteer)
 library(ggcorrplot)
 source('../parallelgwas/manhattanPlot.R')
-preds_pctd_features <- read_csv('output/sam3_embedding_pctd_predictions_rf.csv')
+
+theme_use <- theme_minimal() +
+  theme(axis.text.x = element_text(size = 9, color = 'black', margin = margin(0, 0, 0, 0), 
+                                   vjust = 0.5, hjust = 0.5),
+        axis.text.y = element_text(size = 9, color = 'black', vjust = 0, hjust = 0.5),
+        legend.text = element_text(size = 9, color = 'black', vjust = 0.5, hjust = 0.5),
+        plot.title = element_text(size = 9, color = 'black', vjust = 0, hjust = 0.5),
+        plot.subtitle = element_text(size = 9, color = 'black', vjust = 0, hjust = 0.5),
+        text = element_text(size = 9, color = 'black'),
+        legend.position = 'top',
+        line = element_line(color = 'black', linewidth = 1),
+        axis.line.x.bottom = element_line(color = 'black', linewidth = 0.5),
+        axis.line.y.left = element_line(color = 'black', linewidth = 0.5),
+        panel.grid = element_blank())
+
+preds_pctd_features <- read_csv('output/rf/predictions/sam3_embedding_pctd_all_predictions_rf.csv')
 spearman_r2 <- cor(preds_pctd_features[['label']], preds_pctd_features[['predicted']], use = 'complete.obs', method = 'spearman')^2
 sam3_preds_plot <- ggplot(preds_pctd_features, aes(label, predicted)) + 
-  geom_abline(color = paletteer_d('fishualize::Chlorurus_microrhinos', 2)[2], size = 1) +
-  # geom_bin2d() + 
-  geom_point(color = paletteer_d('fishualize::Chlorurus_microrhinos', 1)) +
+  geom_point(color = paletteer_d('MetBrewer::Archambault', 1)) +
   annotate(geom = 'text', x = 10, y=35, label = str_c('R^2==', spearman_r2), parse = TRUE) +
-  # scale_fill_viridis(direction = -1) +
-  # guides(fill = guide_colorbar(barwidth = 10)) +
-  labs(x = 'Observed Percent Diseased Area\n(ExG Threshold)', 
+  scale_x_continuous(expand = c(0, 0), limits = c(0, 90)) + 
+  scale_y_continuous(expand = c(0, 0), limits = c(0, 40)) +
+  labs(x = 'Percent Diseased Area\n(ExG Threshold)', 
        y = 'Predicted Percent Disease Area\n(RF)', 
-       title = 'SAM3\nEmbeddings') + 
+       title = 'SAM3\nEmbedding Means & Standard Deviations') + 
   theme_use 
 sam3_preds_plot
 ggsave('output/sam3_pred_accuracy.png', dpi = 2000, plot = sam3_preds_plot)
@@ -23,12 +36,14 @@ high_err_preds <- preds_pctd_features %>%
   filter(label > 75) %>% 
   filter(predicted < 10) 
 
-fi_pctd_features <- read_csv('output/sam3_embedding_pctd_feature_importances_rf.csv')[, 2:1025] %>% 
+fi_pctd_features <- read_csv('output/rf/sam3_rs_embedding_pctd_senesced_removed_feature_importances_rf.csv')%>% 
   pivot_longer(cols = everything(), names_to = 'feature', values_to = 'fi') %>% 
   group_by(feature) %>%
   summarise(avg_fi = mean(fi, na.rm = TRUE)) %>% 
   arrange(desc(avg_fi)) %>% 
-  mutate(feature = as.numeric(feature))
+  mutate(feature = as.numeric(feature)) %>% 
+  mutate(stat = case_when(feature < 1024 ~ 'mean', .default = 'std'), 
+         embedding_num = case_when(feature > 1023 ~ feature - 1024, .default = feature))
 
 fi_plot <- ggplot(fi_pctd_features, aes(avg_fi)) + 
   geom_histogram(fill = paletteer_d('MetBrewer::Archambault', 6)[3]) +
@@ -45,9 +60,9 @@ high_fi <- fi_pctd_features %>%
   arrange(desc(avg_fi))
 high_fi <- high_fi[1:30, ]
 
-high_fi_features <- str_c('embedding_', high_fi$feature)
+high_fi_features <- str_c('embedding', high_fi$stat, high_fi$embedding_num, sep = '_')
 
-embeddings <- read_csv('output/sam3_rf_predictors.csv')
+embeddings <- read_csv('output/rf/sam3_rs_rf_predictors.csv')
 high_fi_embeddings <- embeddings %>% 
   select(any_of(high_fi_features)) %>% 
   as.matrix()
@@ -63,7 +78,7 @@ p <- ggcorrplot(cor_mat,
   theme(axis.text.x = element_text(angle = 90))
 print(p)
 
-feature_blues <- read_csv('output/sam3_blues.csv')
+feature_blues <- read_csv('output/sam3_rs_blues.csv')
 
 for(feature in high_fi_features)
 {
@@ -84,23 +99,26 @@ write_csv(blues_winsor, 'output/sam3_high_fi_blues_winsorized.csv')
 
 for(feature in high_fi_features)
 {
-  samplePhenotypesForResampling('output/sam3_high_fi_blues_winsorized.csv', 'genotype', feature)
+  samplePhenotypesForResampling('output/sam3_rs_blues.csv', 'genotype', feature)
 }
 
 # all_farmcpu_hits <- summariseSignals_PANICLE('output/gwas/sam3/farmcpu/GWAS_embedding*') %>%
-#   mutate(feature = str_split_i(filename, '_', 3) %>% 
-#            as.numeric(), 
-#          iter = str_split_i(filename, '_', 4) %>% 
+#   mutate(stat = str_split_i(filename, '_', 3),
+#          feature = str_split_i(filename, '_', 4) %>%
+#            as.numeric(),
+#          iter = str_split_i(filename, '_', 5) %>%
 #            as.numeric())
 # write_csv(all_farmcpu_hits, 'output/sam3_high_fi_allfarmcpuhits.csv')
 
 all_farmcpu_hits <- read_csv('output/sam3_high_fi_allfarmcpuhits.csv')
 
 rmip <- all_farmcpu_hits %>% 
-  group_by(SNP, CHROM, POS, feature) %>% 
+  group_by(SNP, CHROM, POS, feature, stat) %>% 
   summarise(RMIP = n()/100, 
             min_p = min(pval, na.rm = TRUE), 
-            mean_effect = mean(effect, na.rm = TRUE))
+            mean_effect = mean(effect, na.rm = TRUE)) %>% 
+  mutate(embedding = str_c('embedding', stat, feature, sep = '_')) %>% 
+  arrange(desc(RMIP))
 
 rmip_0.1features <- rmip %>% 
   ungroup() %>% 
@@ -115,15 +133,17 @@ rmip_0.1features <- rmip %>%
   mutate(CHROM = str_remove(CHROM, 'Chr') %>% 
            as.numeric())
 
+select_features <- intersect(head(rmip$embedding, n=15), head(high_fi_features, n=15))
+n_features <- length(select_features)
 rmip_selected <- rmip %>% 
-  filter(feature %in% c(560, 986, 586, 344, 174, 122)) %>% 
+  filter(embedding %in% select_features) %>% 
   ungroup() %>% 
   mutate(CHROM = str_remove(CHROM, 'Chr') %>% 
-           as.numeric())
+           as.numeric(), 
+         label = str_c(feature, '\n(', str_to_title(stat), ')'))
 
-plotManhattan(rmip_selected, RMIP, multitrait = TRUE, trait = feature, threshold = 0.2, 
-              colors = paletteer_d('MetBrewer::Archambault', 6),
-              species = 'sorghum')
+plotManhattan(rmip_selected, RMIP, multitrait = TRUE, trait = label, threshold = 0.2, 
+              colors = paletteer_d('MetBrewer::Archambault', n_features),
+              species = 'sorghum', theme = theme_use)
 ggsave('output/selected_embeddings_farmcpu.png', width = 5, height = 2.5, dpi = 1000, 
        bg = 'transparent')
-
--- src/autoencoder/segment_leaf_visualize_steps.py
+++ src/autoencoder/segment_leaf_visualize_steps.py
@@ -57,7 +57,7 @@ def largest_component_touching_sides(binary_mask: np.ndarray) -> tuple[int | Non
 
     return best_label, (labels == best_label)
 
-image_path ='data/ne2025/device7/2281_LeafPhotoA_2025-09-08 16_59_13.027-05_00.jpg'
+image_path ='output/1622_LeafPhotoA_2025-09-09 09_56_01.433-05_00.jpg'
 tolerance1=50
 tolerance2=50
 down_from_top=750
