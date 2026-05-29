d257b1fab0ca9c9c0121a2c66df2da2f42cce335
jdavis-132 <jdavis132@huskers.unl.edu>
2026-05-29 12:27:51 -0500

    update lowfi highH image grids

--- src/R/sampleBLUEsForResampling.R
+++ src/R/sampleBLUEsForResampling.R
@@ -9,11 +9,21 @@ high_fi_features <- c("embedding_std_930", "embedding_std_552", "embedding_std_9
                       "embedding_mean_950", "embedding_mean_119", "embedding_std_128", "embedding_mean_139", "embedding_std_76", "embedding_std_839",
                       "embedding_std_326", "embedding_mean_842", "embedding_std_567", "embedding_mean_237", "embedding_std_793")
 
+lv_cols_b <- c("embedding_std_976", "embedding_std_552", "embedding_std_930", "embedding_mean_637", "embedding_std_918", "embedding_std_383",
+             "embedding_mean_586", "embedding_mean_968", "embedding_mean_886", "embedding_mean_656", "embedding_mean_698", "embedding_mean_210",
+             "embedding_mean_836", "embedding_std_968", "embedding_std_132", "embedding_mean_37",  "embedding_mean_68", "embedding_std_687",
+             "embedding_std_793",  "embedding_mean_165", "embedding_mean_582", "embedding_std_821",  "embedding_mean_108", "embedding_mean_119",
+             "embedding_mean_989", "embedding_std_839", "embedding_mean_514", "embedding_mean_546", "embedding_std_606", "embedding_mean_984",
+             "embedding_mean_792", "embedding_std_981", "embedding_std_270", "embedding_std_594", "embedding_mean_437", "embedding_mean_901",
+             "embedding_mean_129", "embedding_std_567", "embedding_mean_930", "embedding_std_983",  "embedding_mean_139", "embedding_std_166",
+             "embedding_mean_214", "embedding_std_82",  "embedding_mean_109", "embedding_mean_734", "embedding_std_517")
+high_fi_features <- setdiff(lv_cols_b, high_fi_features)
+
 blues <- read_csv('output/sam3_blues.csv')
-splitDataFrame(blues, contains('embedding'), 'output/sam3_blues_', 100)
+# splitDataFrame(blues, contains('embedding'), 'output/sam3_blues_', 100)
 
 for(f in high_fi_features)
 {
   samplePhenotypesForResampling('output/sam3_blues.csv', genotype = 'genotype', trait = f, prop_keep = 0.9, n_samples = 100,
-                                samples_per_file = 100)
+                                samples_per_file = 10)
 }
